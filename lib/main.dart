1089a426f4fe465db203df8209d237fe7b128ffa
twizeyimana <twizelissa@gmail.com>
2022-02-20 19:41:04 +0200

    adding widget and button

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,18 @@ void main() => runApp(MyFirstApp());
 class MyFirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = ['What\'s Your Favorite Food', 'What\'s Your blood DNA'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Demo App'),
+          title: Text('IQ TEST APP'),
         ),
-        body: Text('Hello Android Developer'),
+        body: Column(
+          children:[
+            Text('Questions!'),
+            RaisedButton(child: Text,)
+          ],
+        ),
       ),
     );
   }
