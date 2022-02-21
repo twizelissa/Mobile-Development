58b5923964860f7063d810fceac1319b85035790
twizeyimana <twizelissa@gmail.com>
2022-02-21 15:52:32 +0200

    changed state from  stateless to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,59 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyFirstApp());
 
-class MyFirstApp extends StatelessWidget {
+class MyFirstApp extends StatefulWidget {
+  //connecting generic class with statful class
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyFirstAppState();
+  }
+}
+
+//state is generic class
+class MyFirstAppState extends State<MyFirstApp> {
+  var quetionIndex = 0;
   void answerQuestion() {
-    print('Quetions answered');
+    setState(() {
+      quetionIndex = quetionIndex + 1;
+    });
+    print(quetionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = ['What\'s Your Favorite Food', 'What\'s Your blood DNA'];
+    var questions = [
+      'Do you love Reading',
+      'Do you Believe Hard work paysoff',
+      'What is your favorite food',
+      'What do you do in your free time',
+      'Do you care Improvement of other',
+      'Did you sometime think that you fucked off',
+      'If yes in previous question when'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            'NO BRAIN APP',
+            'IQ TEST APP',
           ),
         ),
         body: Column(
           children: [
-            Text('Questions!'),
+            Text(questions[quetionIndex]),
             RaisedButton(child: Text('Question 1'), onPressed: answerQuestion),
-            RaisedButton(child: Text('Question 2'), onPressed: answerQuestion),
-            RaisedButton(child: Text('Question 3'), onPressed: answerQuestion),
+            RaisedButton(
+                child: Text('Question 2'),
+                onPressed: () => print('Question 2 answered')),
+            RaisedButton(
+              child: Text('Question 3'),
+              onPressed: () {
+                print("Question 3 answered");
+              },
+            ),
           ],
         ),
       ),
