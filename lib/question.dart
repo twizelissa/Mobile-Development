952a7b6629855b547372710feebde357740212a5
twizeyimana <twizelissa@gmail.com>
2022-02-22 13:44:02 +0200

    added style to our widget

--- lib/question.dart
+++ lib/question.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 class Question extends StatelessWidget {
   final String questionText;
   Question(this.questionText);
-  
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      questionText,
-      style: TextStyle(fontSize: 25),
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(20),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 20),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
