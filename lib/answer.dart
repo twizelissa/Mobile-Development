a7498a786714cf24d56b29e7a8cbd65911339fbd
twizeyimana <twizelissa@gmail.com>
2022-02-23 11:49:10 +0200

    error fixed

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,16 +1,18 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
   final String answerText;
-  Answer(this.selectHandler, this.answerText);
+   Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
       child: RaisedButton(
-        color: Color.fromARGB(255, 54, 32, 65),
+        color: const Color.fromARGB(255, 54, 32, 65),
         textColor: Colors.white,
         child: Text(answerText),
         onPressed: selectHandler,
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import './answer.dart';
 void main() => runApp(MyFirstApp());
 
 class MyFirstApp extends StatefulWidget {
+  const MyFirstApp({Key? key}) : super(key: key);
+
   //connecting generic class with statful class
   @override
   State<StatefulWidget> createState() {
@@ -15,48 +17,57 @@ class MyFirstApp extends StatefulWidget {
 //state is generic class
 //_MyFirstAppState the reason for underscore is to  make it private
 class _MyFirstAppState extends State<MyFirstApp> {
-  var _quetionIndex = 0;
+  final questions = const [
+    {
+      'questionText': 'What is your favorite color ?',
+      'answers': ['blue', 'green', 'orange', 'red'],
+    },
+    {
+      'questionText': 'What is your favorite food ?',
+      'answers': ['fish', 'chips', 'meat', 'rice'],
+    },
+    {
+      'questionText': 'What is your favorite football star ?',
+      'answers': [
+        'Ronaldo',
+        'Messi',
+        'Lewandowski',
+        'Mbape',
+        'Neymar',
+        'Haland'
+      ],
+    },
+    {
+      'questionText': 'What is your favorite pet ?',
+      'answers': ['Cat', 'Dog', 'Corocodile', 'Snake'],
+    },
+  ];
+
+  var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
-      _quetionIndex = _quetionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(_quetionIndex);
+    // ignore: avoid_print
+    print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('We have more question');
+    } else {
+      print('No question lef');
+      
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What is your favorite color ?',
-        'answers': ['blue', 'green', 'orange', 'red'],
-      },
-      {
-        'questionText': 'What is your favorite food ?',
-        'answers': ['fish', 'chips', 'meat', 'rice'],
-      },
-      {
-        'questionText': 'What is your favorite football star ?',
-        'answers': [
-          'Ronaldo',
-          'Messi',
-          'Lewandowski',
-          'Mbape',
-          'Neymar',
-          'Haland'
-        ],
-      },
-      {
-        'questionText': 'What is your favorite pet ?',
-        'answers': ['Cat', 'Dog', 'Corocodile', 'Snake'],
-      },
-    ];
+    // ignore: unnecessary_const
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 54, 32, 65),
-          title: Text(
+          backgroundColor: const Color.fromARGB(255, 54, 32, 65),
+          title: const Text(
             'SKILLGAIN',
           ),
           centerTitle: true,
@@ -64,9 +75,9 @@ class _MyFirstAppState extends State<MyFirstApp> {
         body: Column(
           children: [
             Question(
-              questions[_quetionIndex]['questionText'] as String,
+              questions[_questionIndex]['questionText'] as String,
             ),
-            ...(questions[_quetionIndex]['answers'] as List<String>).map(
+            ...(questions[_questionIndex]['answers'] as List<String>).map(
               (answer) {
                 return Answer(_answerQuestion, answer);
               },
