9dc8b255b1839e7b8ede80e5c0054124f42a9a45
twizeyimana <twizelissa@gmail.com>
2022-02-24 21:00:53 +0200

    added result text

--- lib/result.dart
+++ lib/result.dart
@@ -4,11 +4,24 @@ class Result extends StatelessWidget {
   final int resultScore;
 
   Result(this.resultScore);
+
+  String get resultPhase {
+    var resultText;
+    if (resultScore >= 80) {
+      resultText = 'You are Genius';
+    } else if (resultScore < 80 && resultScore > 50) {
+      resultText = 'Good,You did you best';
+    } else {
+      resultText = 'Don\'t worry the life doen\'t end here';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Text(
-      'You did it!',
+      resultPhase,
       style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
     ));
   }
